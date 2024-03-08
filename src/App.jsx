6b9f8b8a7765068f297c8f3f615124a16c466c1a
Kutay Şahin <79334889+kutaysahindev@users.noreply.github.com>
2024-03-08 16:12:14 +0300

    Add Services.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Benefits from "./components/Benefits";
-import Button from "./components/Button";
 import Collaboration from "./components/Collaboration";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Services from "./components/Services";
 
 const App = () => {
   return (
@@ -13,6 +13,7 @@ const App = () => {
         <Hero />
         <Benefits />
         <Collaboration />
+        <Services />
       </div>
       <ButtonGradient />
     </>
