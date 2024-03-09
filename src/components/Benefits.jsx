311bdac733fda3db7cd58c12bde69a5a174a348e
Kutay Şahin <79334889+kutaysahindev@users.noreply.github.com>
2024-03-09 21:19:33 +0300

    Add pricing section

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -10,7 +10,7 @@ const Benefits = () => {
     <Section id="features">
       <div className="container relative z-2">
         <Heading
-          className="md:max-w-md lg:max-w-2xl"
+          className="md:max-w-md lg:max-w-2xl text-center"
           title="Chat Smarter, Not Harder with Brainwave"
         />
 
